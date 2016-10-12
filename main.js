1fae21f509805bfa49fa777315d04149b829e6cb
Joel Harris <joel@fortyau.com>
2016-10-11 23:04:02 -0500

    created the logic for checking for a large straight

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ var diceJar = {
   "dice4": {value:null, rollable:true},
   "dice5": {value:null, rollable:true},
 }
-var rollCount = 0
+
+
+
 //- Create a "roll" function, keeps track of roll count
+var rollCount = 0
 var roll = function() {
   if (rollCount == 3){
     for(var di in diceJar){
@@ -23,6 +26,7 @@ var roll = function() {
   rollCount++
   }
 }
+
 // - Create a "hold die" function
 var hold = function(diToHold, holdno) {
     diceJar[diToHold].rollable = !diceJar[diToHold].rollable;
@@ -33,11 +37,101 @@ var hold = function(diToHold, holdno) {
       document.getElementById(holdno).innerHTML = "Hold";
     }
 }
-// - Create a "Score Points" function
-var scorePoints = function(scorename){
-  switch scorename = {
 
-    "yahtzee": loop over the dice values and if all the dice are equal then add 50 points to the points section
+//
+//  #    # ###### #      #####  ###### #####     #    # ###### ##### #    #  ####  #####   ####
+//  #    # #      #      #    # #      #    #    ##  ## #        #   #    # #    # #    # #
+//  ###### #####  #      #    # #####  #    #    # ## # #####    #   ###### #    # #    #  ####
+//  #    # #      #      #####  #      #####     #    # #        #   #    # #    # #    #      #
+//  #    # #      #      #      #      #   #     #    # #        #   #    # #    # #    # #    #
+//  #    # ###### ###### #      ###### #    #    #    # ######   #   #    #  ####  #####   ####
+//
+
+// helps us get the sum of the dice
+function get_sum(total, num){
+  return total + num
+}
+
+// takes the value of each dice and puts it into an array we can manipulate
+map_dice = function(){
+  var dice_array = [2,3,4,4,5]
+
+  // for(di in diceJar){
+  //   dice_array.push(diceJar[di].value)
+  // }
+
+  return dice_array
+}
+
+// this method combines two functions, map_dice and sort
+release_the_orderer_of_the_dice = function(){
+
+  // we need to move our dice values into a usuable array
+  current_jar = map_dice()
+
+  // we then need to sort them to check and see if we have the right dice, depending on the score,
+  // but doesnt hurt to do this anyway
+  current_jar.sort(function(a, b){return a-b})
+
+  // we then return the the current jar variable to the function where it's called, this is abstraction
+  // we can use this to our benefit to make our functions universal and usuable in multiple places without repeating code
+  return current_jar
+}
+
+
+//   #####   #####  ####### ######  #######    ####### ######     #     #####  #    # ####### ######
+//  #     # #     # #     # #     # #             #    #     #   # #   #     # #   #  #       #     #
+//  #       #       #     # #     # #             #    #     #  #   #  #       #  #   #       #     #
+//   #####  #       #     # ######  #####         #    ######  #     # #       ###    #####   ######
+//        # #       #     # #   #   #             #    #   #   ####### #       #  #   #       #   #
+//  #     # #     # #     # #    #  #             #    #    #  #     # #     # #   #  #       #    #
+//   #####   #####  ####### #     # #######       #    #     # #     #  #####  #    # ####### #     #
+//
+
+// - Create a "Score Points" function
+var current_score = 0
+var scorePoints = function(scorename){
+
+  // we take in the type of points you want to score in the switch
+  switch (scorename){
+
+    // it matches to the lg straight, so it runs this block.
+    case 'lg_straight':
+
+      // for all these cases, we will assume that the user passes the test and will fail them later if they don't meet criteria
+      passes_test = true
+
+      // create a local variable that from helper method that gives us an ordered array of our dice
+      ordered_dice = release_the_orderer_of_the_dice()
+
+      // So we know in a straight that all 5 dice have to be consecutive, whether that is 1,2,3,4,5 or 2,3,4,5,6. the roll must contain
+      // one of these sets of numbers in order to score the points.
+      // to do this we will loop through the ordered_dice array and see if the current di(iteration) + 1 is equal
+      // to the next di in the array. If it's not we flip the test to false and break the loop because we know they failed the test for a straight
+      // else we just continue through the loop until it's over
+      for(var di = 0; di<ordered_dice.length; di++){
+        // we need to check for undefined here because, if 5 or six is the last number if we do +1 on it, it won't know what we're talking about
+        if(ordered_dice[di+1] == undefined){
+          break
+        }
+        else if(ordered_dice[di]+1 != ordered_dice[di+1]){
+          passes_test = false
+          break
+        }
+        else{
+          continue
+        }
+        di++
+      }
+
+      // if the test is still true, then give the guy some points
+      if(passes_test){
+        current_score += 40
+      }
+
+      // reset the dom score
+      document.getElementById("score").innerHTML = current_score;
+
   }
 
 }
