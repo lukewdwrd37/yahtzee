337be371ef4f28b22f6995e8c0983bab803f6f48
Luke Woodward <luke.wdwrd@gmail.com>
2016-10-25 10:14:03 -0500

    Added all score functions

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var diceJar = {
   "dice5": {value:null, rollable:true},
 }
 
-
 //- Create a "roll" function, keeps track of roll count
 var rollCount = 0
 var roll = function() {
@@ -30,7 +29,7 @@ var roll = function() {
 var hold = function(diToHold, holdno) {
     diceJar[diToHold].rollable = !diceJar[diToHold].rollable;
     if (!diceJar[diToHold].rollable){
-      document.getElementById(holdno).innerHTML = "*";
+      document.getElementById(holdno).innerHTML = "[O]";
     }
     else{
       document.getElementById(holdno).innerHTML = "Hold";
@@ -49,7 +48,7 @@ var hold = function(diToHold, holdno) {
 //
 
 // helps us get the sum of the dice
-function get_sum(total, num){
+get_sum = function(total, num){
   return total + num;
 }
 
@@ -90,13 +89,13 @@ release_the_tallyman = function(){
 
     if (tallymans_tallyboard.hasOwnProperty(current_jar[di])){
       tallymans_tallyboard[current_jar[di]] += 1
-    }
-    else{
+    } else {
       tallymans_tallyboard[current_jar[di]] = 1
     }
   }
   debugger
   return tallymans_tallyboard
+  console.log(tallymans_tallyboard);
 }
 
 
@@ -115,8 +114,8 @@ var scorePoints = function(scorename){
 
   // we take in the type of points you want to score in the switch
   switch (scorename){
-    case 'yahtzee':
 
+    case 'yahtzee':
       var theGrail = release_the_tallyman();
       for(var i in theGrail) {
         if (theGrail[i] == 5){
@@ -124,16 +123,15 @@ var scorePoints = function(scorename){
           document.getElementById("score").innerHTML = current_score;
         }
       }
-      break
+      break;
+
     // it matches to the lg straight, so it runs this block.
     case 'lg_straight':
 
       // for all these cases, we will assume that the user passes the test and will fail them later if they don't meet criteria
       passes_test = true
-
       // create a local variable that from helper method that gives us an ordered array of our dice
       ordered_dice = release_the_orderer_of_the_dice()
-
       // So we know in a straight that all 5 dice have to be consecutive, whether that is 1,2,3,4,5 or 2,3,4,5,6.
       // the roll must contain one of these sets of numbers in order to score the points.
       // to do this we will loop through the ordered_dice array and see if the current di(iteration) + 1 is equal
@@ -142,11 +140,11 @@ var scorePoints = function(scorename){
       for(var di = 0; di<ordered_dice.length; di++){
         // we need to check for undefined here because, if 5 or six is the last number and we do +1 on it, it won't know what we're talking about
         if(ordered_dice[di+1] == undefined){
-          break
+          break;
         }
         else if(ordered_dice[di]+1 != ordered_dice[di+1]){
           passes_test = false
-          break
+          break;
         }
         else{
           continue
@@ -156,13 +154,47 @@ var scorePoints = function(scorename){
 
       // if the test is still true, then give the guy some points
       if(passes_test){
-        current_score += 40
+        current_score += 40;
       }
 
       // reset the dom score
       document.getElementById("score").innerHTML = current_score;
-
       break;
+
+    case 'sm_straight':
+
+          // create a local variable that from helper method that gives us an ordered array of our dice
+          ordered_dice = release_the_orderer_of_the_dice()
+
+          // So we know in a straight that all 5 dice have to be consecutive, whether that is 1,2,3,4,5 or 2,3,4,5,6.
+          // the roll must contain one of these sets of numbers in order to score the points.
+          // to do this we will loop through the ordered_dice array and see if the current di(iteration) + 1 is equal
+          // to the next di in the array. If it's not we flip the test to false and break the loop because we know they failed the test for a straight
+          // else we just continue through the loop until it's over
+          for(var di = 0; di<ordered_dice.length; di++){
+            // we need to check for undefined here because, if 5 or six is the last number and we do +1 on it, it won't know what we're talking about
+            if(ordered_dice[di+1] == undefined){
+              break;
+            }
+            else if(ordered_dice == 1,2,3,4 || 2,3,4,5 || 3,4,5,6) {
+              passes_test = true
+              break
+            }
+            else{
+              continue
+            }
+            di++
+          }
+          // if the test is still true, then give the guy some points
+          if(passes_test){
+            current_score += 30;
+          }
+
+          // reset the dom score
+          document.getElementById("score").innerHTML = current_score;
+
+          break;
+
     case 'full_house':
 
       // release the tallyman gives me an object that has the dice values as keys and a tally of how many of each i have
@@ -185,18 +217,102 @@ var scorePoints = function(scorename){
 
       // reset the dom score
       document.getElementById("score").innerHTML = current_score + 25;
-
       break;
 
-  }
+    case '4_ofaKind':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if (diceOrder[i] == 4) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+        }
+      }
+      break;
 
-}
+    case '3_ofaKind':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if (diceOrder[i] == 3) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
 /*
-       #       #       # ####### ####### #           #####  #     # ####### #       #        #####
-      #       #        # #     # #       #          #     # ##   ## #       #       #       #     #
-     #       #         # #     # #       #          #       # # # # #       #       #       #
-    #       #          # #     # #####   #           #####  #  #  # #####   #       #        #####
-   #       #     #     # #     # #       #                # #     # #       #       #             #
-  #       #      #     # #     # #       #          #     # #     # #       #       #       #     #
- #       #        #####  ####### ####### #######     #####  #     # ####### ####### #######  #####
+       #       #  #####  ### #     #  #####  #       #######  #####
+      #       #  #     #  #  ##    # #     # #       #       #     #
+     #       #   #        #  # #   # #       #       #       #
+    #       #     #####   #  #  #  # #  #### #       #####    #####
+   #       #           #  #  #   # # #     # #       #             #
+  #       #      #     #  #  #    ## #     # #       #       #     #
+ #       #        #####  ### #     #  #####  ####### #######  #####
  */
+
+// Replicating the method that adds the amount of each dice for 'single points'
+
+    case 'ones':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 1) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
+    case 'twos':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 2) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
+    case 'threes':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 3) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
+    case 'fours':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 4) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
+    case 'fives':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 5) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+
+    case 'sixes':
+      var diceOrder = release_the_tallyman();
+      for(var i in diceOrder) {
+        if ([i] == 6) {
+          current_score += diceOrder[i]*[i];
+          document.getElementById("score").innerHTML = current_score;
+          }
+      }
+      break;
+  }
+/*  if (button){
+    for(var di in diceJar){
+        diceJar[di].rollable == 0;
+      }*/
+}
